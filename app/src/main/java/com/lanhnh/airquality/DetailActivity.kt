406870494e45264ce2621f71831db5708b2fad16
anhmv-0866 <mai.van.anh@sun-asterisk.com>
2020-05-09 23:41:42 +0700

    add recyclerview

--- app/src/main/java/com/lanhnh/airquality/DetailActivity.kt
+++ app/src/main/java/com/lanhnh/airquality/DetailActivity.kt
@@ -2,32 +2,42 @@ package com.lanhnh.airquality
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.lanhnh.airquality.data.Air
-import com.lanhnh.airquality.data.model.Device
+import com.lanhnh.airquality.screen.air.AirAdapter
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     private var title: String = "device-id-1"
     private var itemList = mutableListOf<Air>()
+    private var airAdapter: AirAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-//        val bundle = intent.extras
-//        if (bundle != null) {
-//            title = bundle.getString(MainActivity.EXTRA_TITLE, "")
-//        }
+        val bundle = intent.extras
+        if (bundle != null) {
+            title = bundle.getString(MainActivity.EXTRA_TITLE, "")
+        }
+        airAdapter = AirAdapter(this, itemList) { air ->
+            itemClick(air)
+        }
+
+        airAdapter?.let {
+            rv_air_quality.apply {
+                adapter = it
+            }
+        }
 
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("data").child(title)
+        val myRef = database.getReference("data").child(title).orderByKey()
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 dataSnapshot.children.forEach {
@@ -38,11 +48,15 @@ class DetailActivity : AppCompatActivity() {
                     val airQuality = it.child("data-device").child("1").value
                     itemList.add(Air(id, deviceId, dust, airQuality.toString().toDouble(), time))
                 }
-                Log.d("DetailActivity", "Count " + itemList.count())
+                airAdapter?.setData(itemList)
             }
 
             override fun onCancelled(error: DatabaseError) {
             }
         })
+    }
+
+    private fun itemClick(air: Air?) {
+        Toast.makeText(this, air?.time ?: "", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/lanhnh/airquality/screen/air/AirAdapter.kt
+++ app/src/main/java/com/lanhnh/airquality/screen/air/AirAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.lanhnh.airquality.R
 import com.lanhnh.airquality.data.Air
@@ -13,11 +14,21 @@ class AirAdapter(
     private val context: Context,
     private var listData: MutableList<Air>,
     private var onItemClick: ((Air?) -> Unit)?
-) :
-    RecyclerView.Adapter<AirAdapter.Companion.AirItem>() {
+) : RecyclerView.Adapter<AirAdapter.Companion.AirItem>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AirItem {
         val view = LayoutInflater.from(context).inflate(R.layout.item, parent, false)
         return AirItem(view, onItemClick)
+    }
+
+    fun setData(data: MutableList<Air>) {
+        listData.clear()
+        listData.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    public fun insert(data: Air, position: Int = FIRST) {
+        listData.add(position, data)
+        notifyItemRangeInserted(position, 1)
     }
 
     override fun getItemCount(): Int {
@@ -29,6 +40,10 @@ class AirAdapter(
     }
 
     companion object {
+        const val FIRST = 0
+        const val WARNING_DUST = 2
+        const val WARNING_AIR_QUALITY = 2
+
         class AirItem(itemView: View, private var onItemClick: ((Air?) -> Unit)?) :
             RecyclerView.ViewHolder(itemView) {
 
@@ -37,7 +52,10 @@ class AirAdapter(
                     tv_time.text = item.time
                     tv_dust.text = item.dust.toString()
                     tv_air_quality.text = item.airQuality.toString()
-
+                    ll_bg_item.background =
+                        if (item.dust >= WARNING_DUST || item.airQuality >= WARNING_AIR_QUALITY)
+                            ContextCompat.getDrawable(context, R.drawable.bg_orange)
+                        else ContextCompat.getDrawable(context, R.drawable.bg_purple)
                     setOnClickListener {
                         onItemClick?.invoke(item)
                     }
